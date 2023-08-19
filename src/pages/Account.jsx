ea821c366697e61fd9c1b477dc591b5e68b4fb76
Syed <syednaqvi7863@gmail.com>
2023-08-19 16:10:50 -0400

    init commit

---
+++ src/pages/Account.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import SavedShows from "../components/SavedShows";
+
+const Account = () => {
+  return (
+    <>
+      <div className="w-full text-white">
+        <img
+          className="w-full h-[400px] object-cover"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/955e84a0-6be6-482c-8d45-126279f53895/US-en-20230814-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
+          alt="/"
+        />
+        <div className="bg-black/60 fixed top-0 left-0 w-full h-[550px]">
+          <div className="absolute top-[20%] p-4 md:p-8">
+            <h1 className="text-3xl md:text-5xl font-bold">My Shows</h1>
+          </div>
+        </div>
+      </div>
+      <SavedShows />
+    </>
+  );
+};
+
+export default Account;
